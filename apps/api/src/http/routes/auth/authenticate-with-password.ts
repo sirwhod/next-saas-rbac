71e4785168b50f5f07d80f1ab9c2c74b09457568
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-10-30 17:55:22 -0300

    class: Rota: Perfil do usuário logado

--- apps/api/src/http/routes/auth/authenticate-with-password.ts
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -15,6 +15,14 @@ export async function authenticateWithPassword(app: FastifyInstance) {
           email: z.string().email(),
           password: z.string(),
         }),
+        response: {
+          400: z.object({
+            message: z.string(),
+          }),
+          201: z.object({
+            token: z.string(),
+          }),
+        },
       },
     },
     async (request, reply) => {
